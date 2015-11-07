b2637b1af694cf52a47d39b3228065033c88d20e
Josh Sephton <josh.sephton@gmail.com>
2015-11-07 21:53:55 +0000

    test: Bucketer requests if already bucketed from the session

--- test/bucketer.js
+++ test/bucketer.js
@@ -10,4 +10,19 @@ describe("Bucketer", () => {
 		assert.instanceOf(bucketer, Bucketer);
 	});
 
+	it('requests if already bucketed from the session', () => {
+		var session = { variant: function () {} };
+
+		var experiment = 'experiment-1';
+		var variant = 'control';
+
+		var mock = sinon.mock(session);
+		mock.expects("variant").withArgs(experiment).once().returns(variant);
+
+		var bucketer = new Bucketer(experiment, session);
+		assert.equal(bucketer.variant(), variant);
+
+		mock.verify();
+	});
+
 });
