0818e2a086b3d8a4ce386da111fea5cfbc8b231a
Josh Sephton <josh.sephton@gmail.com>
2015-11-07 21:58:44 +0000

    feat: Bucketer requests if already bucketed from the session

--- src/bucketer.js
+++ src/bucketer.js
@@ -1,5 +1,13 @@
 export class Bucketer {
 
-	//
+	constructor(experiment, session) {
+		this.experiment = experiment;
+		this.session = session;
+	}
+
+	variant() {
+		var variant = this.session.variant(this.experiment);
+		return variant;
+	}
 
 }
