af5150054adb7903147c5ec2319074008cfb4b83
Josh Sephton <josh.sephton@gmail.com>
2015-11-07 22:25:49 +0000

    test: Bucketer assigns to bucket if not already bucketed

--- test/bucketer.js
+++ test/bucketer.js
@@ -19,10 +19,73 @@ describe("Bucketer", () => {
 		var mock = sinon.mock(session);
 		mock.expects("variant").withArgs(experiment).once().returns(variant);
 
-		var bucketer = new Bucketer(experiment, session);
+		var bucketer = new Bucketer(experiment, [], session);
 		assert.equal(bucketer.variant(), variant);
 
 		mock.verify();
+	});
+
+	it('assigns to bucket if not already bucketed (control)', () => {
+		var session = { variant: function () {} };
+
+		var experiment = 'experiment-1';
+		var variant = 'control';
+		var variants = ['control', 'variant-1', 'variant-2'];
+
+		var mock = sinon.mock(session);
+		mock.expects("variant").withArgs(experiment).once().returns(false);
+
+		var stub = sinon.stub(Math, 'random');
+		stub.returns(0.2);
+
+		var bucketer = new Bucketer(experiment, variants, session);
+		assert.equal(bucketer.variant(), variant);
+
+		mock.verify();
+
+		stub.restore();
+	});
+
+	it('assigns to bucket if not already bucketed (variant-1)', () => {
+		var session = { variant: function () {} };
+
+		var experiment = 'experiment-1';
+		var variant = 'variant-1';
+		var variants = ['control', 'variant-1', 'variant-2'];
+
+		var mock = sinon.mock(session);
+		mock.expects("variant").withArgs(experiment).once().returns(false);
+
+		var stub = sinon.stub(Math, 'random');
+		stub.returns(0.5);
+
+		var bucketer = new Bucketer(experiment, variants, session);
+		assert.equal(bucketer.variant(), variant);
+
+		mock.verify();
+
+		stub.restore();
+	});
+
+	it('assigns to bucket if not already bucketed (variant-2)', () => {
+		var session = { variant: function () {} };
+
+		var experiment = 'experiment-1';
+		var variant = 'variant-2';
+		var variants = ['control', 'variant-1', 'variant-2'];
+
+		var mock = sinon.mock(session);
+		mock.expects("variant").withArgs(experiment).once().returns(false);
+
+		var stub = sinon.stub(Math, 'random');
+		stub.returns(0.8);
+
+		var bucketer = new Bucketer(experiment, variants, session);
+		assert.equal(bucketer.variant(), variant);
+
+		mock.verify();
+
+		stub.restore();
 	});
 
 });
