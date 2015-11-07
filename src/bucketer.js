01bad475651ae545491147f5348cb2fe24a64070
Josh Sephton <josh.sephton@gmail.com>
2015-11-07 22:25:57 +0000

    feat: Bucketer assigns to bucket if not already bucketed

--- src/bucketer.js
+++ src/bucketer.js
@@ -1,12 +1,21 @@
 export class Bucketer {
 
-	constructor(experiment, session) {
+	constructor(experiment, variants, session) {
 		this.experiment = experiment;
+		this.variants = variants;
 		this.session = session;
 	}
 
 	variant() {
 		var variant = this.session.variant(this.experiment);
+
+		if (!variant) {
+			var limit = 1/this.variants.length;
+			var random = Math.random();
+			var index = Math.floor(((random * 10) - 0.1) / (limit * 10));
+			variant = this.variants[index];
+		}
+
 		return variant;
 	}
 
