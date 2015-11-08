2c1b257cfbe5575e0caf4ba3444afce25e7598de
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 00:17:55 +0000

    test: Session reads the saved variant from a cookie

--- test/session.js
+++ test/session.js
@@ -1,6 +1,7 @@
 import { assert } from "chai"
 import { Session } from "../src/session.js"
 
+var Cookie = require('js-cookie');
 var sinon = require("sinon");
 
 describe("Session", () => {
@@ -8,6 +9,16 @@ describe("Session", () => {
 	it('can create', () => {
 		var session = new Session;
 		assert.instanceOf(session, Session);
+	});
+
+	it('reads the saved variant from a cookie', () => {
+		var stub = sinon.stub(Cookie, 'get');
+		stub.returns(undefined);
+
+		var session = new Session;
+		assert.isUndefined(session.variant('experiment-1'));
+
+		stub.restore();
 	});
 
 })
