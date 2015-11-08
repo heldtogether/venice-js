493c709d19a3c8544d76b0424ded73e81c92acd5
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:11:59 +0000

    test: Manager loads config if provided

--- test/manager.js
+++ test/manager.js
@@ -1,6 +1,8 @@
 import { assert } from "chai"
 import { Manager } from "../src/manager.js"
 
+var sinon = require("sinon");
+
 describe("Manager", () => {
 
 	it('can create', () => {
@@ -22,6 +24,25 @@ describe("Manager", () => {
 		manager.set(experiment, feature)
 
 		assert.equal(manager.get(experiment), feature);
+	});
+
+	it('loads config if provided', () => {
+		var config1 = { rules: function () {} }
+		var config2 = { rules: function () {} }
+
+		var mock1 = sinon.mock(config1);
+		mock1.expects("rules").once();
+
+		var mock2 = sinon.mock(config2);
+		mock2.expects("rules").once();
+
+		var manager = new Manager();
+		manager.addConfig(config1);
+		manager.addConfig(config2);
+		manager.get('experiment-1');
+
+		mock1.verify();
+		mock2.verify();
 	});
 
 })
