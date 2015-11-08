f03048acf0c3716a6a46823f98992e9fd5ca25aa
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:51:00 +0000

    test: Factory can create feature

--- test/factory.js
+++ test/factory.js
@@ -1,11 +1,25 @@
 import { assert } from "chai"
 import { Factory } from "../src/factory.js"
+import { Feature } from "../src/feature.js"
+
+var definition = {
+	"type": "variant",
+	"variants": [
+		"control",
+		"variant-1"
+	]
+};
 
 describe("Factory", () => {
 
 	it('can create', () => {
 		var factory = new Factory;
 		assert.instanceOf(factory, Factory);
+	});
+
+	it('can create feature', () => {
+		var factory = new Factory;
+		assert.instanceOf(factory.create('experiment-1', definition), Feature);
 	});
 
 });
