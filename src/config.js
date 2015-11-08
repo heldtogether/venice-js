004db3df76073258b954e2b0929fc02494fd8929
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:36:17 +0000

    feat: Config calls the factory for each of the rules

--- src/config.js
+++ src/config.js
@@ -1,11 +1,20 @@
 export class Config {
 
-	constructor() {
+	constructor(factory) {
 		this._definition = {};
+		this.factory = factory;
 	}
 
 	setDefintion(definition) {
 		this._definition = definition;
+	}
+
+	rules() {
+		var rules = {};
+		for (var feature in this._definition) {
+			rules[feature] = this.factory.create(this._definition[feature]);
+		}
+		return rules;
 	}
 
 }
