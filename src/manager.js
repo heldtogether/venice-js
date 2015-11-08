eaad30ec4e770f8d4206c683a66832f69d2b1af1
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:12:04 +0000

    feat: Manager loads config if provided

--- src/manager.js
+++ src/manager.js
@@ -1,15 +1,39 @@
 export class Manager {
 
-	constructor() {
+	constructor(config) {
+		this.config = [];
+		this.loadedConfig = false;
 		this._features = {};
 	}
 
+	addConfig(config) {
+		this.config.push(config);
+		this.loadedConfig = false;
+	}
+
 	get(name) {
+		this._loadConfig();
 		return this._features[name];
 	}
 
 	set(name, feature) {
+		this._loadConfig();
 		this._features[name] = feature;
+	}
+
+	_loadConfig() {
+		if (this.config.length !== 0 && !this.loadedConfig) {
+			this._features = {};
+			this.loadedConfig = true;
+			for (var i = 0; i < this.config.length; i++) {
+				var rules = this.config[i].rules();
+				if (rules) {
+					for (rule in rules) {
+						this.set(rule, rules[rule]);
+					}
+				}
+			}
+		}
 	}
 
 }
