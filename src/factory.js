bb66be6fd3eed1e2692474c5c3bbdef3506f9106
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:51:13 +0000

    feat: Factory can create feature

--- src/factory.js
+++ src/factory.js
@@ -1,5 +1,17 @@
+import { Bucketer } from "../src/bucketer.js"
+import { Feature } from "../src/feature.js"
+import { Session } from "../src/session.js"
+
 export class Factory {
 
-	//
+	create(experiment, definition) {
+		var session = new Session;
+
+		var variants = definition['variants'];
+		var bucketer = new Bucketer(experiment, variants, session);
+
+		var feature = new Feature(bucketer);
+		return feature;
+	}
 
 }
