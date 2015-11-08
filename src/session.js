8c62bb008c71b08a7158b9da336a75a83c236b0f
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:14:52 +0000

    style: whitespace

--- src/session.js
+++ src/session.js
@@ -3,15 +3,11 @@ var Cookie = require('js-cookie');
 export class Session {
 
 	variant(experiment) {
-
 		return Cookie.get(experiment);
-
 	}
 
 	setVariant(experiment, variant) {
-
 		Cookie.set(experiment, variant);
-
 	}
 
 }
