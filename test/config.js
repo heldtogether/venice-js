05526f5d7fbe9e0f6e1f3f874dc153fb1a1e6b96
Josh Sephton <josh.sephton@gmail.com>
2015-11-08 01:26:52 +0000

    test: Config can set definition

--- test/config.js
+++ test/config.js
@@ -8,4 +8,19 @@ describe("Config", () => {
 		assert.instanceOf(config, Config);
 	});
 
+	it('can set definition', () => {
+		var definition = {
+			"experiment-1": {
+				"variants": [
+					"control",
+					"variant-1"
+				]
+			}
+		};
+
+		var config = new Config;
+		config.setDefintion(definition);
+		assert.equal(config._definition, definition);
+	});
+
 });
