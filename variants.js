65d4ebc87f2a29ab7498f911637cf3d35658e5e4
Josh Sephton <josh@383project.com>
2015-11-09 10:58:33 +0000

    feat: export all modules from single file

---
+++ variants.js
@@ -0,0 +1,24 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+
+var _bucketerJs = require('./lib/bucketer.js');
+
+var _configJs = require('./lib/config.js');
+
+var _factoryJs = require('./lib/factory.js');
+
+var _featureJs = require('./lib/feature.js');
+
+var _managerJs = require('./lib/manager.js');
+
+var _sessionJs = require('./lib/session.js');
+
+exports.Bucketer = _bucketerJs.Bucketer;
+exports.Config = _configJs.Config;
+exports.Factory = _factoryJs.Factory;
+exports.Feature = _featureJs.Feature;
+exports.Manager = _managerJs.Manager;
+exports.Session = _sessionJs.Session;
