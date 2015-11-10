f21a081b564744cf2739eca4bb84e2903f724c3d
Josh Sephton <josh@383project.com>
2015-11-10 08:53:33 +0000

    feat: add init method to library for configuring dependencies

--- venice.js
+++ venice.js
@@ -5,15 +5,10 @@ Object.defineProperty(exports, '__esModule', {
 });
 
 var _bucketerJs = require('./lib/bucketer.js');
-
 var _configJs = require('./lib/config.js');
-
 var _factoryJs = require('./lib/factory.js');
-
 var _featureJs = require('./lib/feature.js');
-
 var _managerJs = require('./lib/manager.js');
-
 var _sessionJs = require('./lib/session.js');
 
 exports.Bucketer = _bucketerJs.Bucketer;
@@ -22,3 +17,16 @@ exports.Factory = _factoryJs.Factory;
 exports.Feature = _featureJs.Feature;
 exports.Manager = _managerJs.Manager;
 exports.Session = _sessionJs.Session;
+
+exports.init = function(definition) {
+	var session = new _sessionJs.Session();
+
+	var factory = new _factoryJs.Factory(session);
+
+	var config = new _configJs.Config(factory);
+	config.setDefintion(definition);
+
+	var manager = new _managerJs.Manager();
+	manager.addConfig(config);
+	return manager;
+};
